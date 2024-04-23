2fdd501c06011750cb2b555653f6e19b01ac597d
Kate <kit-ty-kate@outlook.com>
2024-04-23 23:23:27 +0100

    Finish the torrent file parser

--- src/torrent_file.ml
+++ src/torrent_file.ml
@@ -1,49 +1,77 @@
-type ('a, 'b) length_or_files =
-  | Length of 'a
-  | Files of 'b
+type file = {
+  length : int64;
+  path : string list;
+}
+
+type length_or_files =
+  | Length of int64
+  | Files of file list
 
 type t = {
   announce : string;
   name : string;
   piece_length : int64;
   pieces : Sha1.t list;
-  length_or_files : (int64, unit) length_or_files;
+  length_or_files : length_or_files;
 }
+
+exception Parse_error
 
 let parse file =
   let bt = Bencode.decode (`File_path file) in
   match bt with
-  | Integer _ -> assert false
-  | String _ -> assert false
-  | List _ -> assert false
+  | Integer _ -> raise Parse_error
+  | String _ -> raise Parse_error
+  | List _ -> raise Parse_error
   | Dict bt ->
       let announce = List.assoc_opt "announce" bt in
       let info = List.assoc_opt "info" bt in
       match announce, info with
-      | None, _ -> assert false
-      | _, None -> assert false
+      | None, _ -> raise Parse_error
+      | _, None -> raise Parse_error
       | Some (String announce), Some (Dict info) ->
           let name = List.assoc_opt "name" info in
           let piece_length = List.assoc_opt "piece length" info in
           let pieces = List.assoc_opt "pieces" info in
           let length_or_files =
             match List.assoc_opt "length" info, List.assoc_opt "files" info with
-            | None, None -> assert false
-            | Some _, Some _ -> assert false
-            | Some length, None -> Length length
-            | None, Some files -> Files files
+            | None, None -> raise Parse_error
+            | Some _, Some _ -> raise Parse_error
+            | Some (Integer length), None -> Length length
+            | None, Some (List files) ->
+                let files =
+                  List.map (function
+                    | Bencode.Integer _ -> raise Parse_error
+                    | Bencode.String _ -> raise Parse_error
+                    | Bencode.List _ -> raise Parse_error
+                    | Bencode.Dict file ->
+                        let length = List.assoc_opt "length" file in
+                        let path = List.assoc_opt "path" file in
+                        match length, path with
+                        | None, _ -> raise Parse_error
+                        | _, None -> raise Parse_error
+                        | Some (Integer length), Some (List path) ->
+                            let path =
+                              List.map (function
+                                | Bencode.Integer _ -> raise Parse_error
+                                | Bencode.String s -> s
+                                | Bencode.List _ -> raise Parse_error
+                                | Bencode.Dict _ -> raise Parse_error
+                              ) path
+                            in
+                            {length; path}
+                        | Some _, Some _ -> raise Parse_error
+                  ) files
+                in
+                Files files
+            | Some _, None -> raise Parse_error
+            | None, Some _ -> raise Parse_error
           in
           begin match name, piece_length, pieces with
-          | None, _, _ -> assert false
-          | _, None, _ -> assert false
-          | _, _, None -> assert false
+          | None, _, _ -> raise Parse_error
+          | _, None, _ -> raise Parse_error
+          | _, _, None -> raise Parse_error
           | Some (String name), Some (Integer piece_length), Some (String pieces) ->
-              let length_or_files = match length_or_files with
-                | Length (Integer length) -> Length length
-                | Files (List _) -> failwith "TODO"
-                | Length _ -> assert false
-                | Files _ -> assert false
-              in
               let pieces =
                 List.init (String.length pieces / 20) (fun i ->
                   let n = i * 20 in
@@ -58,9 +86,9 @@ let parse file =
                 pieces;
                 length_or_files;
               }
-          | Some _, Some _, Some _ -> assert false
+          | Some _, Some _, Some _ -> raise Parse_error
           end
-      | Some _, Some _ -> assert false
+      | Some _, Some _ -> raise Parse_error
 
 let print {announce; name; piece_length; pieces; length_or_files} =
   print_endline ("announce: " ^ announce);
@@ -69,5 +97,9 @@ let print {announce; name; piece_length; pieces; length_or_files} =
   print_endline "pieces:";
   List.iter (fun hash -> print_endline ("- " ^ Sha1.to_hex hash)) pieces;
   match length_or_files with
-  | Length length -> print_endline ("length: " ^ Int64.to_string length)
-  | Files _ -> failwith "TODO"
+  | Length length ->
+      print_endline ("length: " ^ Int64.to_string length);
+  | Files files ->
+      List.iter (fun {length; path} ->
+        print_endline (String.concat "/" path ^ ": " ^ Int64.to_string length);
+      ) files
